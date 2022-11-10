4096dd668e9ec022c5623ac4806f27ae798c37b8
Dan O'Dea <dansbands@hotmail.com>
2022-11-10 10:00:49 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  console.log("App Loaded!");
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>This is my attempt to push this repo to Github</h2>
+    </div>
+  );
+}
